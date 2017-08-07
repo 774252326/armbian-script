9bebe7bd52655660883795b80c07cebd07636e70
u <u@16.04b>
2017-08-07 14:45:19 +0800

    opencv contrib text

--- opencvin.sh
+++ opencvin.sh
@@ -1,6 +1,6 @@
 # sudo apt-get install libopencv-dev
-sudo apt-get install build-essential gcc g++ gdb cmake cmake-gui pkg-config git qt5-default qtcreator -y
-sudo apt-get install libvtk5-dev python python-dev python-numpy libgtk2.0-dev libavcodec-dev libavformat-dev libswscale-dev libjpeg-dev libpng-dev libtiff-dev libjasper-dev libdc1394-22-dev libgphoto2-dev libeigen3-dev libgflags-dev libgoogle-glog-dev libprotobuf-dev libboost-all-dev libhdf5-serial-dev protobuf-compiler doxygen libgtk-3-dev libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev libgstreamer1.0-0-dbg libavresample-dev libsuitesparse-dev -y
+sudo apt-get install build-essential gcc g++ gdb cmake cmake-gui pkg-config git qt5-default qtcreator automake -y
+sudo apt-get install libvtk5-dev python python-dev python-numpy libgtk2.0-dev libavcodec-dev libavformat-dev libswscale-dev libjpeg-dev libpng-dev libtiff-dev libjasper-dev libdc1394-22-dev libgphoto2-dev libeigen3-dev libgflags-dev libgoogle-glog-dev libprotobuf-dev libboost-all-dev libhdf5-serial-dev protobuf-compiler doxygen libgtk-3-dev libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev libgstreamer1.0-0-dbg libavresample-dev libsuitesparse-dev libleptonica-dev libtesseract-dev -y
 sudo apt-get autoremove -y
 
 git clone https://ceres-solver.googlesource.com/ceres-solver
