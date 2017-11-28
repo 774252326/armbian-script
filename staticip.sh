a14abd4aed8d53aa4464af000cf66043553ca330
Jian HanBin <jhanbin@lscm.ad>
2017-11-28 09:02:15 +0800

    static ip

---
+++ staticip.sh
@@ -0,0 +1,9 @@
+sudo leafpad /etc/dhcpcd.conf
+
+#Type in the following lines on the top of the file: 
+#interface eth0 
+#static ip_address=192.168.1.XX/24 
+#static routers=192.168.1.1 
+#static domain_name_servers=192.168.1.1
+
+#reboot
