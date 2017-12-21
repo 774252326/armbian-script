b2f8d5c76c49b912e88db71a249a533517911ffc
u <u@nuc.16.04>
2017-12-21 10:15:31 +0800

    clean dukto

--- dukto6.sh
+++ dukto6.sh
@@ -3,3 +3,5 @@ sudo apt-key add - < Release.key
 sudo sh -c "echo 'deb http://download.opensuse.org/repositories/home:/colomboem/xUbuntu_16.04/ /' >> /etc/apt/sources.list.d/dukto.list"
 sudo apt-get update
 sudo apt-get install dukto
+sudo rm -rf ./Release.key
+sudo rm -rf /etc/apt/sources.list.d/dukto.list
