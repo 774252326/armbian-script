8ce82ba411a7cc76c922a1ecfcaef94e08841245
u <u@16.04>
2018-03-12 15:06:19 +0800

    remove raspbian pre install

---
+++ preinstall.sh
@@ -0,0 +1,2 @@
+sudo apt remove -y nodered nuscratch minecraft-pi wolfram-engine sonic-pi libreoffice greenfoot geany epiphany-browser bluej claws-mail claws-mail-i18n python python3 python2.7 scratch scratch2
+sudo apt autoremove --purge -y
